35c18f1d96325955135fc2db0fbc1ac009ef71b0
Mohamed Hamdan <mohamedhamdan@Mohameds-MacBook-Pro.local>
2021-01-31 10:24:03 +0300

    import packages and dummy data

---
+++ src/trips.js
@@ -0,0 +1,48 @@
+const trips = [
+  {
+    name: "Amwaj Trip",
+    image:
+      "https://i1.wp.com/hera-realestate.com/wp-content/uploads/2015/06/Dilmunia-Top-Manmade-Island1.jpg",
+    city: "Amwaj",
+    difficulty: "Hard",
+    length: 8,
+    rating: 1,
+  },
+  {
+    name: "Manama Trip",
+    image:
+      "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/15/33/fb/8b/manama.jpg?w=1100&h=600&s=1",
+    city: "Manama",
+    difficulty: "Easy",
+    length: 1,
+    rating: 5,
+  },
+  {
+    name: "Muharraq Trip",
+    image:
+      "https://cdn4.gttwl.net/attachments/global/1476123159_original.jpeg?w=640&h=480&fit=crop&crop=entropy&auto=format,enhance&q=60",
+    city: "Muharraq",
+    difficulty: "Easy",
+    length: 2,
+    rating: 4,
+  },
+  {
+    name: "Arad Trip",
+    image: "https://upload.wikimedia.org/wikipedia/commons/1/13/AradFort.jpg",
+    city: "Arad",
+    difficulty: "Medium",
+    length: 3,
+    rating: 3,
+  },
+  {
+    name: "Riffa Trip",
+    image:
+      "https://www.weetas.com/insights/wp-content/uploads/2020/05/Riffa.png",
+    city: "Riffa",
+    difficulty: "Medium",
+    length: 5,
+    rating: 2,
+  },
+];
+
+export default trips;
